518dc3671320214fde1b9d1574fce8a409f5789f
Hiba Nelson <bolomolo025@gmail.com>
2026-03-05 12:08:15 +0200

    added Object datatype and Object

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
-let userName: string = "Mohab";
-console.log(userName);
+// create object datatype
+type userDataType = {
+    readonly name: string;
+    age: number;
+    isAdmin?: boolean;
+}
+let user1: userDataType = {
+    name: "John",
+    age: 30,
+    isAdmin: true
+}
+// user1.name = "Jane"; // Error: Cannot assign to 'name' because it is a read-only property.
+user1.age = 31; // Allowed: age is not read-only 
+let user2: userDataType = {
+    name: "Alice",
+    age: 25
+}  
